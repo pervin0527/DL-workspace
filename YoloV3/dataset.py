d9a7146bca439d2dfa89600ffdc9ad85c5267486
pervin0527 <zfbtldnz@gmail.com>
2023-07-05 22:40:16 +0900

    Updated 2023.07.05

--- YoloV3/dataset.py
+++ YoloV3/dataset.py
@@ -1,12 +1,15 @@
 import torch
-import numpy as np
+import random
 import xml.etree.ElementTree as ET
 
 from tqdm import tqdm
 from PIL import Image
 from torch.utils.data import Dataset
-from torchvision import transforms
 from torch.nn import functional as F
+
+
+def resize(image, size):
+    return F.interpolate(image.unsqueeze(0), size, mode='bilinear', align_corners=True).squeeze(0)
 
 
 def generate_letter_box_image(image, fill_value=0):
@@ -28,12 +31,18 @@ def generate_letter_box_image(image, fill_value=0):
 
 
 class DetectionDataset(Dataset):
-    def __init__(self, data_dir, set_name, annot_type=None, year=None, image_size=416, transform=None):
+    def __init__(self, data_dir, set_name, annot_type=None, year=None, image_size=416, transform=None, multiscale=True, normalized_labels=True):
         self.data_dir = data_dir
         self.annot_type = annot_type
         self.set_name = set_name
         self.image_size = image_size
         self.transform = transform
+        self.multiscale = multiscale
+        self.normalized_labels = normalized_labels
+        self.batch_count=0
+
+        with open("./classes.txt", "r") as f:
+            self.classes = sorted([x.strip() for x in f.readlines()])
         
         if annot_type.lower() == "voc":
             if self.set_name == "train":
@@ -50,8 +59,7 @@ class DetectionDataset(Dataset):
             if year != None:
                 self.json_path = f"{data_dir}/annotations/instances_{set_name}{year}.json"
 
-        self.classes, self.dataset = self.get_dataset()
-        self.classes = sorted(self.classes)
+        self.dataset = self.get_dataset()
 
 
     def __len__(self):
@@ -61,43 +69,69 @@ class DetectionDataset(Dataset):
     def __getitem__(self, index):
         data = self.dataset[index]
         file_name = data["file_name"]
-
         image = data["image"]
         targets = data["targets"]
 
-        for i in range(len(targets)):
-            str_label = targets[i][0]
-            targets[i][0] = self.classes.index(str_label)
-
         image = self.transform(image)
-        channel, height, width = image.shape
+        _, h, w = image.shape
+        h_factor, w_factor = (h, w) if self.normalized_labels else (1, 1)
+
         image, pad = generate_letter_box_image(image)
-        _, padded_height, padded_width = image.shape
+        _, padded_h, padded_w = image.shape
 
         bboxes = torch.tensor(targets).reshape(-1, 5)
-        x1 = width * (bboxes[:, 1] - bboxes[:, 3] / 2)
-        y1 = height * (bboxes[:, 2] - bboxes[:, 4] / 2)
-        x2 = width * (bboxes[:, 1] + bboxes[:, 3] / 2)
-        y2 = height * (bboxes[:, 2] + bboxes[:, 4] / 2)
+        # Extract coordinates for unpadded + unscaled image
+        x1 = w_factor * (bboxes[:, 1] - bboxes[:, 3] / 2)
+        y1 = h_factor * (bboxes[:, 2] - bboxes[:, 4] / 2)
+        x2 = w_factor * (bboxes[:, 1] + bboxes[:, 3] / 2)
+        y2 = h_factor * (bboxes[:, 2] + bboxes[:, 4] / 2)
 
+        # Adjust for added padding
         x1 += pad[0]
         y1 += pad[2]
         x2 += pad[1]
         y2 += pad[3]
 
-        bboxes[:, 1] = ((x1 + x2) / 2) / padded_width
-        bboxes[:, 2] = ((y1 + y2) / 2) / padded_height
-        bboxes[:, 3] *= width / padded_width
-        bboxes[:, 4] *= height / padded_height
+        # Returns (x, y, w, h)
+        bboxes[:, 1] = ((x1 + x2) / 2) / padded_w
+        bboxes[:, 2] = ((y1 + y2) / 2) / padded_h
+        bboxes[:, 3] *= w_factor / padded_w
+        bboxes[:, 4] *= h_factor / padded_h
 
         targets = torch.zeros((len(bboxes), 6))
         targets[:, 1:] = bboxes
 
         return file_name, image, targets
+    
+
+    def collate_fn(self, batch):
+        paths, images, targets = list(zip(*batch))
+
+        # Remove empty placeholder targets
+        targets = [boxes for boxes in targets if boxes is not None]
+
+        # Add sample index to targets
+        for i, boxes in enumerate(targets):
+            boxes[:, 0] = i
+
+        try:
+            targets = torch.cat(targets, 0)
+        except RuntimeError:
+            targets = None  # No boxes for an image
+
+        # Selects new image size every 10 batches
+        if self.multiscale and self.batch_count % 10 == 0:
+            self.image_size = random.choice(range(320, 608 + 1, 32))
+
+        # Resize images to input shape
+        images = torch.stack([resize(image, self.image_size) for image in images])
+        self.batch_count += 1
+
+        return paths, images, targets
 
 
     def get_dataset(self):
-        classes, dataset = [], []
+        dataset = []
         if self.annot_type == "voc":
             for idx in tqdm(range(len(self.file_list))):
                 file = self.file_list[idx]
@@ -106,19 +140,16 @@ class DetectionDataset(Dataset):
 
                 image_path = f"{self.data_dir}/JPEGImages/{file}.jpg"
                 image = Image.open(image_path).convert("RGB")
-
-                for label in labels:
-                    if label not in classes:
-                        classes.append(label)
                 
                 targets = []
                 for bbox, label in zip(bboxes, labels):
                     x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
+                    label = self.classes.index(label)
                     targets.append([label, x, y, w, h])
                 
                 dataset.append({"file_name" : file, "image" : image, "targets" : targets})
                 
-        return classes, dataset
+        return dataset
 
 
     def voc_coord_transform(self, width, height, xmin, xmax, ymin, ymax):
@@ -163,9 +194,3 @@ class DetectionDataset(Dataset):
                 bboxes.append([x, y, w, h])
 
         return bboxes, labels, isdifficult
-    
-
-if __name__ == "__main__":
-    transform = transforms.Compose([transforms.ToTensor()])
-    dataset = DetectionDataset("/home/pervinco/Datasets/PASCAL_VOC/VOCdevkit/VOC2012", "train", "voc", transform=transform)
-    print(dataset[1])

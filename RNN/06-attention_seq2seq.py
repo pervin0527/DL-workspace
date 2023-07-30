301ed9e9ccc27ce3eb38aaf6fd143d24f5ee4861
pervin0527 <zfbtldnz@gmail.com>
2023-07-31 02:16:07 +0900

    Updated 2023.07.31

--- RNN/06-attention_seq2seq.py
+++ RNN/06-attention_seq2seq.py
@@ -109,34 +109,46 @@ class EncoderRNN(nn.Module):
         return (torch.zeros(1, 1, self.hidden_size, device=device), torch.zeros(1, 1, self.hidden_size, device=device))
 
     
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, max_length, dropout_p=0.1):
+        super(AttnDecoderRNN, self).__init__()
         self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
 
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.lstm = nn.LSTM(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
+    def forward(self, input, hidden, encoder_outputs): ## hidden
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(self.attn(torch.cat((embedded[0], hidden[0].view(1, 256)), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0), encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
         output = F.relu(output)
         output, hidden = self.lstm(output, hidden)
-        output = self.softmax(self.out(output[0]))
 
-        return output, hidden
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
 
     def initHidden(self):
         # return torch.zeros(1, 1, self.hidden_size, device=device)
-        return (torch.zeros(1, 1, self.hidden_size), torch.zeros(1, 1, self.hidden_size))
+        return (torch.zeros(1, 1, self.hidden_size, device=device), torch.zeros(1, 1, self.hidden_size, device=device))
 
 
 def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length, teacher_force_ratio):
     encoder_hidden = encoder.initHidden()
     encoder_optimizer.zero_grad()
     decoder_optimizer.zero_grad()
-
 
     loss = 0
     encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
@@ -152,16 +164,16 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
     if use_teacher_forcing:
         # Teacher forcing 포함: 목표를 다음 입력으로 전달
         for di in range(target_tensor.size(0)):
-            # decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
-            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            # decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
             loss += criterion(decoder_output, target_tensor[di])
             decoder_input = target_tensor[di]  # Teacher forcing
 
     else:
         # Teacher forcing 미포함: 자신의 예측을 다음 입력으로 사용
         for di in range(target_tensor.size(0)):
-            # decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
-            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            # decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
             topv, topi = decoder_output.topk(1)
             decoder_input = topi.squeeze().detach()  # 입력으로 사용할 부분을 히스토리에서 분리
 
@@ -229,9 +241,10 @@ def evaluate(encoder, decoder, sentence, max_length):
         decoder_hidden = encoder_hidden
 
         decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
         for di in range(max_length):
-            # decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
-            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            decoder_attentions[di] = decoder_attention.data
             topv, topi = decoder_output.data.topk(1)
             if topi.item() == EOS_TOKEN:
                 decoded_words.append('<EOS>')
@@ -241,7 +254,7 @@ def evaluate(encoder, decoder, sentence, max_length):
 
             decoder_input = topi.squeeze().detach()
 
-        return decoded_words
+        return decoded_words, decoder_attention[:di + 1]
     
 
 def evaluateRandomly(encoder, decoder, max_length, n=10):
@@ -249,7 +262,7 @@ def evaluateRandomly(encoder, decoder, max_length, n=10):
         pair = random.choice(pairs)
         print('>', pair[0])
         print('=', pair[1])
-        output_words = evaluate(encoder, decoder, pair[0], max_length)
+        output_words, attentions = evaluate(encoder, decoder, pair[0], max_length)
         output_sentence = ' '.join(output_words)
         print('<', output_sentence)
         print('')
@@ -260,11 +273,11 @@ if __name__ == "__main__":
     DATA_DIR = "/home/pervinco/Datasets/seq2seq/data"
     SOS_TOKEN = 0
     EOS_TOKEN = 1
-    MAX_LENGTH = 10
+    MAX_LENGTH = 100
     HIDDEN_SIZE = 256
     
     PRINT_ITER = 1000
-    ITERATIONS = 100000
+    ITERATIONS = 1000000
     LEARING_RATE = 0.01
     TEACHER_FORCING_RATIO = 0.5
 
@@ -272,6 +285,6 @@ if __name__ == "__main__":
     print(random.choice(pairs))
 
     encoder1 = EncoderRNN(input_lang.n_words, HIDDEN_SIZE).to(device)
-    decoder1 = DecoderRNN(HIDDEN_SIZE, output_lang.n_words).to(device)
-    trainIters(encoder1, decoder1, ITERATIONS, LEARING_RATE, MAX_LENGTH, TEACHER_FORCING_RATIO)
-    evaluateRandomly(encoder1, decoder1, MAX_LENGTH)
+    attn_decoder1 = AttnDecoderRNN(HIDDEN_SIZE, output_lang.n_words, max_length=MAX_LENGTH, dropout_p=0.1).to(device)
+    trainIters(encoder1, attn_decoder1, ITERATIONS, LEARING_RATE, MAX_LENGTH, TEACHER_FORCING_RATIO)
+    evaluateRandomly(encoder1, attn_decoder1, MAX_LENGTH)

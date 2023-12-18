ab76fa26848941941c3585ac61aac462e72c697c
pervin0527 <zfbtldnz@gmail.com>
2023-12-18 20:23:46 +0900

    Updated 2023.12.18

--- VISION/detection/train.py
+++ VISION/detection/train.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 from torch import optim
 from torchsummary import summary
-from torchvision import transforms
 from torch.utils.data import DataLoader
 
 from data.dataset import VOCDataset
@@ -16,22 +15,11 @@ from loss import YoloLoss
 
 from models.yolov1 import Yolov1
 
+from utils.graph import plot_and_save
+from utils.lr_scheduler import LinearWarmupDecayScheduler
 from utils.train_param import read_train_params, save_train_params
 from utils.detection_utils import mean_average_precision, get_bboxes
 
-
-# class Compose(object):
-#     def __init__(self, transforms):
-#         self.transforms = transforms
-
-#     def __call__(self, img, bboxes):
-#         for t in self.transforms:
-#             img, bboxes = t(img), bboxes
-
-#         return img, bboxes
-
-
-# transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
 
 def eval(dataloader, model, criterion, device):
     model.eval()
@@ -90,6 +78,9 @@ def main():
     summary(model, input_size=(3, train_params["img_size"], train_params["img_size"]), device="cpu")
     model.to(device)
 
+    if train_params["use_scheduler"]:
+        scheduler = LinearWarmupDecayScheduler(optimizer, train_params["init_lr"], train_params["max_lr"], train_params["min_lr"], train_params["epochs"], train_params["warmup_epochs"])
+
     save_dir = train_params["save_dir"]
     current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     save_path = f"{save_dir}/{current_time}"
@@ -102,6 +93,9 @@ def main():
     
     save_train_params(save_path, train_params)
 
+    train_losses = []
+    valid_losses = []
+    learning_rates = []
     max_mAP = 0
     total_epochs = train_params["epochs"]
     for epoch in range(total_epochs):
@@ -117,6 +111,9 @@ def main():
         valid_mAP = mean_average_precision(pred_boxes, target_boxes, iou_threshold=train_params["iou_threshold"], box_format="midpoint")
         print(f"Valid Loss : {valid_loss:.4f}, Valid mAP : {valid_mAP:.4f}")
 
+        train_losses.append(train_loss)
+        valid_losses.append(valid_loss)
+
         if valid_mAP > max_mAP:
             print(f"Model saved at epoch {epoch+1} with mAP {max_mAP:.4f} --> {valid_mAP:.4f}")
             max_mAP = valid_mAP
@@ -124,6 +121,12 @@ def main():
         else:
             print(f"mAP {valid_mAP:.4f} did not increase. {max_mAP:.4f}")
 
+        if train_params["use_scheduler"]:
+            scheduler.step()
+
+    plot_and_save(train_losses, 'Training Loss', 'Loss', f'{save_path}/plots/train_loss.png')
+    plot_and_save(valid_losses, 'Validation Loss', 'Loss', f'{save_path}/plots/valid_loss.png')
+    plot_and_save(learning_rates, 'Learning Rate', 'Learning Rate', f'{save_path}/plots/learning_rate.png')
 
 
 if __name__ == "__main__":

75afadf78e41a314fa84808a24c09b5703379447
pervin0527 <zfbtldnz@gmail.com>
2024-01-06 11:52:07 +0900

    Updated 2023.01.06

--- VISION/object_detection/YoloV3/data/util.py
+++ VISION/object_detection/YoloV3/data/util.py
@@ -1,4 +1,5 @@
 import cv2
+import torch
 import numpy as np
 
 def pad_to_square(image, pad_value=0):
@@ -23,18 +24,71 @@ def pad_to_square(image, pad_value=0):
     return image_padded
 
 
-def resize_image_and_boxes(image, boxes, new_size):
-    # 이미지를 new_size로 리사이즈
-    old_size = image.shape[:2]
-    image = cv2.resize(image, new_size)
+def xywh2xyxy(x, img_height, img_width):
+    if isinstance(x, torch.Tensor):
+        boxes = x.new(x.shape)
+    elif isinstance(x, np.ndarray):
+        boxes = np.zeros_like(x)
+    else:
+        raise TypeError("Input must be a PyTorch Tensor or a NumPy array")
 
-    # 바운딩 박스 좌표 조정
+    boxes[:, 0] = (x[:, 0] - x[:, 2] / 2) * img_width   # xmin
+    boxes[:, 1] = (x[:, 1] - x[:, 3] / 2) * img_height  # ymin
+    boxes[:, 2] = (x[:, 0] + x[:, 2] / 2) * img_width   # xmax
+    boxes[:, 3] = (x[:, 1] + x[:, 3] / 2) * img_height  # ymax
+    
+    return boxes
+
+
+def xyxy2xywh(boxes, img_height, img_width):
+    dw, dh = 1. / img_width, 1. / img_height
+
+    x_center = (boxes[:, 0] + boxes[:, 2]) / 2.0
+    y_center = (boxes[:, 1] + boxes[:, 3]) / 2.0
+    width = boxes[:, 2] - boxes[:, 0]
+    height = boxes[:, 3] - boxes[:, 1]
+
+    x_center *= dw
+    y_center *= dh
+    width *= dw
+    height *= dh
+
+    y = np.vstack((x_center, y_center, width, height)).T
+
+    return y
+
+
+def resize_image_and_boxes(image, boxes, new_size):
+    height, width = image.shape[:2]
+    boxes = xywh2xyxy(boxes, height, width)
+
+    image = cv2.resize(image, (new_size, new_size))
     if boxes is not None:
-        x_scale = new_size[0] / old_size[1]
-        y_scale = new_size[1] / old_size[0]
-        boxes[:, 1] = boxes[:, 1] * x_scale
-        boxes[:, 2] = boxes[:, 2] * y_scale
-        boxes[:, 3] = boxes[:, 3] * x_scale
-        boxes[:, 4] = boxes[:, 4] * y_scale
+        x_scale = new_size / width
+        y_scale = new_size / height
+        boxes[:, 0] = boxes[:, 0] * x_scale
+        boxes[:, 1] = boxes[:, 1] * y_scale
+        boxes[:, 2] = boxes[:, 2] * x_scale
+        boxes[:, 3] = boxes[:, 3] * y_scale
+
+    boxes = xyxy2xywh(boxes, new_size, new_size)
 
     return image, boxes
+
+
+def draw_boxes(image, boxes, class_idx, total_classes):
+    height, width = image.shape[:2]
+    for box, label in zip(boxes, class_idx):
+        xmin, ymin, xmax, ymax = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+
+        label_str = f'{total_classes[int(label)]}'
+        cv2.putText(image, label_str, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+
+    # plt.figure(figsize=(8, 8))
+    # plt.imshow(image)
+    # plt.axis('off')  # Hide axis
+    # plt.show()
+        
+    cv2.imwrite("./sample.jpg", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
